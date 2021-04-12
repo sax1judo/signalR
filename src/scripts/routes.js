452b8ea64c1bc2f711069b20f4f7d6a53375c5e1
sax1judo <sax1judo@gmail.com>
2021-04-13 00:02:24 +0200

    Add new routes in routes.js

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -1,15 +1,25 @@
 export const API = {
 	trades: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/trades',
 	getStrategiesProgress: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/',
+	strategiesLimits: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/limits/',
+	getStrategiesParameters: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/parameters/',
+	updateStrategiesParametersSell:
+		'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/parameters/sell/',
+	updateStrategiesParametersBuy:
+		'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/parameters/buy/',
 	strategyStart: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/start/',
 	strategyStop: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategies/stop/',
 	signalRChannel: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategyState',
 	getTrades: 'http://oms-trussinvestments.brazilsouth.cloudapp.azure.com:8080/strategyState',
 	//LOCAL ENVIRONMENT
-// 	trades: 'http://localhost:5000/trades',
-// 	getStrategiesProgress: 'http://localhost:5000/strategies/',
-// 	strategyStart: 'http://localhost:5000/strategies/start/',
-// 	strategyStop: 'http://localhost:5000/strategies/stop/',
-// 	signalRChannel: 'http://localhost:5000/strategyState',
-// 	getTrades: 'http://localhost:5000/strategyState',
+	// trades: 'http://localhost:5000/trades',
+	// getStrategiesProgress: 'http://localhost:5000/strategies/',
+	// strategiesLimits: 'http://localhost:5000/strategies/limits/',
+	// getStrategiesParameters: 'http://localhost:5000/strategies/parameters/',
+	// updateStrategiesParametersSell: 'http://localhost:5000/strategies/parameters/sell/',
+	// updateStrategiesParametersBuy: 'http://localhost:5000/strategies/parameters/buy/',
+	// strategyStart: 'http://localhost:5000/strategies/start/',
+	// strategyStop: 'http://localhost:5000/strategies/stop/',
+	// signalRChannel: 'http://localhost:5000/strategyState',
+	// getTrades: 'http://localhost:5000/strategyState',
 };
