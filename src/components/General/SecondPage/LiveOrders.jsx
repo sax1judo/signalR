0b18c1419ea39f5271aa71f9831d1eb092cda788
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-09-22 20:23:41 +0200

    LiveOrders sorting added.

--- src/components/General/SecondPage/LiveOrders.jsx
+++ src/components/General/SecondPage/LiveOrders.jsx
@@ -3,10 +3,31 @@ import '../../../style/General/SecondPage/LiveOrders.scss';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import { API } from '../../../scripts/routes';
 import Loader from '../Loader';
+import sortIcon from '../../../assets/sortIcon.png';
+import sortAscIcon from '../../../assets/sortIconAsc.png';
 
 const LiveOrders = props => {
 	const [connection, setConnection] = useState(null);
 	const [data, setData] = useState({ liveOrders: [] });
+	const [sortField, setSortField] = useState('');
+	const [sortOrder, setSortOrder] = useState('dsc');
+
+	const compareBy = key => {
+		let reverse = sortOrder === 'asc' ? 1 : -1;
+		sortOrder === 'asc' ? setSortOrder('desc') : setSortOrder('asc');
+		return function (a, b) {
+			if (a[key] < b[key]) return -1 * reverse;
+			if (a[key] > b[key]) return 1 * reverse;
+			return 0;
+		};
+	};
+
+	const sortBy = key => {
+		let arrayCopy = [...data.liveOrders];
+		arrayCopy.sort(compareBy(key));
+		setData({ liveOrders: arrayCopy });
+		setSortField(key);
+	};
 
 	useEffect(() => {
 		const newConnection = new HubConnectionBuilder().withUrl(API.signalRChannel).withAutomaticReconnect().build();
@@ -43,6 +64,7 @@ const LiveOrders = props => {
 							}
 						}
 
+						setSortField('');
 						setData({ liveOrders: newData });
 					});
 				})
@@ -64,7 +86,23 @@ const LiveOrders = props => {
 						<tr className="tableHeaderColor">
 							{Object.keys(data.liveOrders[0]).map((liveOrders, id) => {
 								let title = liveOrders.match(/[A-Z]+(?![a-z])|[A-Z]?[a-z]+|\d+/g).join(' ');
-								return <td key={id}>{title}</td>;
+								return (
+									<th onClick={() => sortBy(liveOrders)} key={id}>
+										{title}
+										{sortField === liveOrders ? (
+											<img
+												style={
+													sortOrder === 'asc'
+														? { height: '1.2rem', float: 'right', transform: 'rotate(180deg)' }
+														: { height: '1.2rem', float: 'right' }
+												}
+												src={sortAscIcon}
+											></img>
+										) : (
+											<img style={{ height: '1.2rem', float: 'right' }} src={sortIcon}></img>
+										)}
+									</th>
+								);
 							})}
 						</tr>
 						{data.liveOrders.map((liveOrders, id) => {
