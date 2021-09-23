3778a3921b12c38f844daebcdff2578c028bc1e7
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-09-23 14:02:15 +0200

    Add buttons functionality in Strategies component.

--- src/components/General/SecondPage/Strategies.jsx
+++ src/components/General/SecondPage/Strategies.jsx
@@ -3,16 +3,120 @@ import '../../../style/General/SecondPage/StrategiesTable.scss';
 import { NavLink } from 'react-router-dom';
 import ComponentWrapper from '../../General/ComponentWrapper';
 import MyVerticallyCenteredModal from '../MyVerticallyCenteredModal';
+import sortIcon from '../../../assets/sortIcon.png';
+import sortAscIcon from '../../../assets/sortIconAsc.png';
 
 const StrategiesTable = props => {
+	const [data, setData] = useState({
+		mockData: [
+			{
+				satus: 'running',
+				strategyName: 'buy ISP',
+				type: 'sell',
+				legOneTicker: 'ISP U21',
+				exchangeOne: 'TT',
+				legTwoTicker: 'ES U21',
+				exchangeTwo: 'IB',
+				quantityLegOne: 12,
+				quantityLegTwo: 33,
+				spreadSet: 12,
+				spreadMkt: 1.75,
+				tickers: [
+					{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
+					{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
+				],
+			},
+			{
+				satus: 'running',
+				strategyName: 'sell ISP',
+				type: 'buy',
+				legOneTicker: 'ISP U21',
+				exchangeOne: 'TT',
+				legTwoTicker: 'ES U21',
+				exchangeTwo: 'IB',
+				quantityLegOne: 2,
+				quantityLegTwo: 3,
+				spreadSet: 2,
+				spreadMkt: 1.75,
+				tickers: [
+					{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
+					{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
+				],
+			},
+			{
+				satus: 'paused',
+				strategyName: 'Buy ISsP',
+				type: 'sell',
+				legOneTicker: 'ISP U21',
+				exchangeOne: 'TT',
+				legTwoTicker: 'ES U21',
+				exchangeTwo: 'IB',
+				quantityLegOne: 28,
+				quantityLegTwo: 37,
+				spreadSet: 62,
+				spreadMkt: 1.75,
+				tickers: [
+					{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
+					{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
+				],
+			},
+			{
+				satus: 'paused',
+				strategyName: 'Buy ISPr',
+				type: 'buy',
+				legOneTicker: 'ISP U21',
+				exchangeOne: 'IB',
+				legTwoTicker: 'ES U21',
+				exchangeTwo: 'IB',
+				quantityLegOne: 29,
+				quantityLegTwo: 13,
+				spreadSet: 12,
+				spreadMkt: 21.75,
+				tickers: [
+					{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
+					{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
+				],
+			},
+			{
+				satus: 'stoped',
+				strategyName: 'SELL ISP',
+				type: 'buy',
+				legOneTicker: 'ISP U21',
+				exchangeOne: 'TT',
+				legTwoTicker: 'ES U21',
+				exchangeTwo: 'IB',
+				quantityLegOne: 2,
+				quantityLegTwo: 3,
+				spreadSet: 2,
+				spreadMkt: 3.75,
+				tickers: [],
+			},
+		],
+	});
 	const [selectedStrategies, setSelectedStrategies] = useState([]);
 	const [selectedStrategiesObject, setSelectedStrategiesObject] = useState([]);
 	const [modalShow, setModalShow] = useState(false);
+	const [sortField, setSortField] = useState('');
+	const [sortOrder, setSortOrder] = useState('dsc');
 
 	useEffect(() => {
 		console.log(selectedStrategiesObject);
 	}, [selectedStrategies, selectedStrategiesObject]);
 
+	const selectByType = type => {
+		let allStrategies = [];
+		for (let strategy of data.mockData) {
+			if (strategy.type === type) allStrategies.push(strategy.strategyName);
+		}
+		setSelectedStrategies(allStrategies);
+	};
+	const selectAllStrategies = () => {
+		let allStrategies = [];
+		for (let strategy of data.mockData) {
+			allStrategies.push(strategy.strategyName);
+		}
+		setSelectedStrategies(allStrategies);
+	};
 	const selectStrategy = (strategy, strategyObject) => {
 		let strategies = [];
 		let strategiesObject = [];
@@ -40,6 +144,21 @@ const StrategiesTable = props => {
 		ticker.classList.toggle('expanded');
 		ticker.classList.toggle('collapsed');
 	};
+	const compareBy = key => {
+		let reverse = sortOrder === 'asc' ? 1 : -1;
+		sortOrder === 'asc' ? setSortOrder('desc') : setSortOrder('asc');
+		return function (a, b) {
+			if (a[key] < b[key]) return -1 * reverse;
+			if (a[key] > b[key]) return 1 * reverse;
+			return 0;
+		};
+	};
+	const sortBy = key => {
+		let arrayCopy = [...data.mockData];
+		arrayCopy.sort(compareBy(key));
+		setData({ mockData: arrayCopy });
+		setSortField(key);
+	};
 	return (
 		<div className="secondPageStrategyTable">
 			<table>
@@ -48,12 +167,28 @@ const StrategiesTable = props => {
 						<th colSpan="12">Strategies</th>
 					</tr>
 					<tr className="tableHeaderColor">
-						{Object.keys(props.mockData[0]).map((strategy, id) => {
+						{Object.keys(data.mockData[0]).map((strategy, id) => {
 							let title = strategy.match(/[A-Z]+(?![a-z])|[A-Z]?[a-z]+|\d+/g).join(' ');
-							return <td key={id}>{title}</td>;
+							return (
+								<td onClick={() => sortBy(strategy)} key={id}>
+									{title}
+									{sortField === strategy ? (
+										<img
+											style={
+												sortOrder === 'asc'
+													? { height: '1.2rem', float: 'right', transform: 'rotate(180deg)' }
+													: { height: '1.2rem', float: 'right' }
+											}
+											src={sortAscIcon}
+										></img>
+									) : (
+										<img style={{ height: '1.2rem', float: 'right' }} src={sortIcon}></img>
+									)}
+								</td>
+							);
 						})}
 					</tr>
-					{props.mockData.map((strategy, id) => {
+					{data.mockData.map((strategy, id) => {
 						return (
 							<ComponentWrapper>
 								<tr
@@ -123,16 +258,16 @@ const StrategiesTable = props => {
 				</tbody>
 			</table>
 			<div className="buttonsActionsWrapper">
-				<button type="button" className="btn  addStrategyButton">
+				<button type="button" className="btn addStrategyButton" onClick={() => selectAllStrategies()}>
 					Select All
 				</button>
-				<button type="button" className="btn  addStrategyButton">
+				<button type="button" className="btn  addStrategyButton" onClick={() => setSelectedStrategies([])}>
 					Unselect All
 				</button>
-				<button type="button" className="btn  addStrategyButton">
+				<button type="button" className="btn  addStrategyButton" onClick={() => selectByType('buy')}>
 					Select All Buys
 				</button>
-				<button type="button" className="btn  addStrategyButton">
+				<button type="button" className="btn  addStrategyButton" onClick={() => selectByType('sell')}>
 					Select All Sells
 				</button>
 				<button
--- src/components/Pages/SecondPage.jsx
+++ src/components/Pages/SecondPage.jsx
@@ -5,149 +5,19 @@ import LiveOrders from '../General/SecondPage/LiveOrders';
 import ConnectionMonitoring from '../General/SecondPage/ConnectionMonitoring';
 
 const SecondPage = props => {
-	const createdStrategies = [
-		{
-			satus: 'running',
-			strategyName: 'Buy ISP',
-			type: 'sell',
-			legOneTicker: 'ISP U21',
-			exchangeOne: 'TT',
-			legTwoTicker: 'ES U21',
-			exchangeTwo: 'IB',
-			quantityLegOne: 2,
-			quantityLegTwo: 3,
-			spreadSet: 2,
-			spreadMkt: 1.75,
-			tickers: [
-				{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
-				{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
-			],
-		},
-		{
-			satus: 'running',
-			strategyName: 'sell ISP',
-			type: 'Buy',
-			legOneTicker: 'ISP U21',
-			exchangeOne: 'TT',
-			legTwoTicker: 'ES U21',
-			exchangeTwo: 'IB',
-			quantityLegOne: 2,
-			quantityLegTwo: 3,
-			spreadSet: 2,
-			spreadMkt: 1.75,
-			tickers: [
-				{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
-				{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
-			],
-		},
-		{
-			satus: 'paused',
-			strategyName: 'Buy ISsP',
-			type: 'Buy',
-			legOneTicker: 'ISP U21',
-			exchangeOne: 'TT',
-			legTwoTicker: 'ES U21',
-			exchangeTwo: 'IB',
-			quantityLegOne: 2,
-			quantityLegTwo: 3,
-			spreadSet: 2,
-			spreadMkt: 1.75,
-			tickers: [
-				{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
-				{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
-			],
-		},
-		{
-			satus: 'paused',
-			strategyName: 'Buy ISPr',
-			type: 'Buy',
-			legOneTicker: 'ISP U21',
-			exchangeOne: 'IB',
-			legTwoTicker: 'ES U21',
-			exchangeTwo: 'IB',
-			quantityLegOne: 2,
-			quantityLegTwo: 3,
-			spreadSet: 2,
-			spreadMkt: 1.75,
-			tickers: [
-				{ tickerLegOne: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegOne: 10.0 },
-				{ tickerLegTwo: 'ISP U21', bid: 4255.0, ask: 41212.33, maxLimitLegTwo: 10.0 },
-			],
-		},
-		{
-			satus: 'stoped',
-			strategyName: 'SELL ISP',
-			type: 'Buy',
-			legOneTicker: 'ISP U21',
-			exchangeOne: 'TT',
-			legTwoTicker: 'ES U21',
-			exchangeTwo: 'IB',
-			quantityLegOne: 2,
-			quantityLegTwo: 3,
-			spreadSet: 2,
-			spreadMkt: 1.75,
-			tickers: [],
-		},
-	];
-	const liveOrders = [
-		{
-			time: '11:40:01',
-			status: 'Live',
-			type: 'Buy',
-			strategyName: 'Buy ISP',
-			ticker: 'ES U21',
-			quantity: 2,
-			quantityFilled: 0,
-			price: 4250.0,
-			averagePrice: 2,
-		},
-		{
-			time: '11:40:01',
-			status: 'Live',
-			type: 'Buy',
-			strategyName: 'sell ISP',
-			ticker: 'ES U21',
-			quantity: 2,
-			quantityFilled: 50,
-			price: 4250.0,
-			averagePrice: 2,
-		},
-		{
-			time: '11:40:01',
-			status: 'Live',
-			type: 'Buy',
-			strategyName: 'Buyy ISP',
-			ticker: 'ES U21',
-			quantity: 2,
-			quantityFilled: 10,
-			price: 4250.0,
-			averagePrice: 2,
-		},
-		{
-			time: '11:40:01',
-			status: 'Live',
-			type: 'Buy',
-			strategyName: 'Sell ISP',
-			ticker: 'ES U21',
-			quantity: 2,
-			quantityFilled: 30,
-			price: 4250.0,
-			averagePrice: 2,
-		},
-	];
 	return (
 		<div className="strategiesSecondPageWrapper">
 			<div className="futuresArbitrageStrategies">
 				<h4 style={{ textAlign: 'center' }}>Futures Arbitrage Monitoring</h4>
 				<ConnectionMonitoring />
 				<div>
-					<StrategiesTable mockData={createdStrategies} />
+					<StrategiesTable />
 				</div>
 			</div>
 			<div className="liveOrdersWrapper">
 				<h4 style={{ textAlign: 'center' }}>Live Orders</h4>
 				<div>
-					<LiveOrders mockData={liveOrders} />
+					<LiveOrders />
 				</div>
 			</div>
 		</div>
