3a52db054d51a51de193e29f77f966ef8a0ce5d0
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-10-06 23:07:44 +0200

    Fix DropDowm on pagination to be 10 as start value.

--- src/components/General/DropDown.jsx
+++ src/components/General/DropDown.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../style/General/DropDown.scss';
 
 const DropDown = props => {
 	const recordsPerPage = [5, 10, 15, 30, 50];
+	const [state, setstate] = useState(5);
+	
+	useEffect(() => {
+		setstate(props.postsPerPage);
+	}, []);
+	
 
 	return (
 		<div className="dropDowmWrapper" style={{ marginBottom: '1rem' }}>
 			<select
-				onChange={e => props.setPostsPerPage(e.target.value)}
+				onChange={e => {
+					setstate(parseFloat(e.target.value));
+					props.setPostsPerPage(e.target.value);
+				}}
 				className="selectpicker page-item "
 				aria-label="Default select example"
+				value={state}
 			>
 				{recordsPerPage.map((nmb, index) => {
 					return (
