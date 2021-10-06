75f8262f331679194c1f5d900559db69d3905eb2
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-10-06 23:06:59 +0200

    Header hide/show on scroll up/down.

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../style/General/Header.scss';
 import { NavLink } from 'react-router-dom';
 import ComponentWrapper from './ComponentWrapper';
 
 const Header = () => {
 	const [state, setState] = useState('');
-
+	const lastScroll = useRef(0);
 	useEffect(() => {
 		if (window.innerWidth < 1000) setState('mobile');
 		else setState('desktop');
@@ -13,15 +13,36 @@ const Header = () => {
 			if (window.innerWidth < 1000) setState('mobile');
 			else setState('desktop');
 		});
+
+		window.addEventListener('scroll', () => {
+			const currentScroll = window.pageYOffset;
+			const scrollUp = 'scroll-up';
+			const scrollDown = 'scroll-down';
+			let headerElem = document.getElementById('header');
+			if (currentScroll <= 0) {
+				headerElem.classList.remove(scrollUp);
+				return;
+			}
+			if (currentScroll > lastScroll.current && !headerElem.classList.contains(scrollDown)) {
+				// down
+				headerElem.classList.remove(scrollUp);
+				headerElem.classList.add(scrollDown);
+				// lottiePlayer.play();
+			} else if (currentScroll < lastScroll.current && headerElem.classList.contains(scrollDown)) {
+				// up
+				headerElem.classList.remove(scrollDown);
+				headerElem.classList.add(scrollUp);
+				// lottiePlayer.stop();
+			}
+			lastScroll.current = currentScroll;
+		});
 	}, []);
-	useEffect(() => {
-	
-	}, [state]);
+	useEffect(() => {}, [state]);
 
 	return (
 		<ComponentWrapper>
 			{state === 'desktop' ? (
-				<div className="header">
+				<div id="header" className="header">
 					<div className="logo">
 						<NavLink exact to="/"></NavLink>
 					</div>
