5319042cf173a8050e39b5f0bc8e82bda7b4bb26
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-12-29 12:28:16 +0100

    Add new components in new tabs.

--- src/components/Pages/FifthPage.jsx
+++ src/components/Pages/FifthPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { API } from '../../scripts/routes';
 import { httpRequest } from '../../scripts/http';
+import AuctionTickersTable from '../General/FifthPage/AuctionTickersTable';
 
 const FifthPage = props => {
 	
@@ -8,16 +9,16 @@ const FifthPage = props => {
 	return (
 		<div className="strategiesSecondPageWrapper">
 			<div className="futuresArbitrageStrategies">
-				<h4 style={{ textAlign: 'center' }}>Auctions Monitoring</h4>
-				{/* <AuctionsTickersTable /> */}
+				<h4 style={{ textAlign: 'center' }}>Auctions Arbitrage Monitoring</h4>
+				<AuctionTickersTable />
 				<div>
-					{/* <AuctionsTable arbitrageSpread={arbitrageSpread} arbitrageQuantity={arbitrageQuantity} /> */}
+					{/* <AuctionTable arbitrageSpread={arbitrageSpread} arbitrageQuantity={arbitrageQuantity} /> */}
 				</div>
 			</div>
 			<div className="liveOrdersWrapper">
-				<h4 style={{ textAlign: 'center' }}>Auctions Live Orders</h4>
+				<h4 style={{ textAlign: 'center' }}>Auction Live Orders</h4>
 				<div>
-					{/* <AuctionsLiveOrders /> */}
+					{/* <AuctionLiveOrders /> */}
 				</div>
 			</div>
 		</div>
