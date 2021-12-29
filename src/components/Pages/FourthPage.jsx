6d891aa21e3ea6b4b35c1581fb37941fbd9a214c
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2021-12-29 12:28:16 +0100

    Add new components in new tabs.

--- src/components/Pages/FourthPage.jsx
+++ src/components/Pages/FourthPage.jsx
@@ -1,22 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import { API } from '../../scripts/routes';
+import StockTickersTable from '../General/FourthPage/StockTickersTable';
 
 const FourthPage = props => {
 	return (
 		<div className="strategiesSecondPageWrapper">
 			<div className="futuresArbitrageStrategies">
-				<h4 style={{ textAlign: 'center' }}>Stocks Monitoring</h4>
-				{/* <StockTickersTable /> */}
-				<div>
-					{/* <StocksTable arbitrageSpread={arbitrageSpread} arbitrageQuantity={arbitrageQuantity} /> */}
-				</div>
+				<h4 style={{ textAlign: 'center' }}>Stocks Arbitrage Monitoring</h4>
+				<StockTickersTable />
+				<div>{/* <StockTable arbitrageSpread={arbitrageSpread} arbitrageQuantity={arbitrageQuantity} /> */}</div>
 			</div>
 			<div className="liveOrdersWrapper">
-				<h4 style={{ textAlign: 'center' }}>Stocks Live Orders</h4>
-				<div>
-					{/* <StocksLiveOrders /> */}
-				</div>
+				<h4 style={{ textAlign: 'center' }}>Stock Live Orders</h4>
+				<div>{/* <StockLiveOrders /> */}</div>
 			</div>
 		</div>
 	);
