c04e76428eec87beefc93a9bacb835f90106e552
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2022-01-31 21:17:47 +0100

    New function getUniquea added in common.js

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,5 +1,19 @@
+//ENUMS
 export const loadActionPages = [
 	{ pageNumber: 1, pageName: 'Arbitrage Monitoring', strategyType: 'Arbitrage' },
 	{ pageNumber: 2, pageName: 'Stock Monitoring', strategyType: 'Stock' },
 	{ pageNumber: 3, pageName: 'Auction Monitoring', strategyType: 'Auction' },
 ];
+//FUNCTIONS
+export const getUnique = array => {
+	let uniqueArray = [];
+
+	// Loop through array values
+	for (let i = 0; i < array.length; i++) {
+		if (uniqueArray.indexOf(array[i]) === -1) {
+			uniqueArray.push(array[i]);
+		}
+	}
+	return uniqueArray;
+};
+
