f1876224638fc4c511deadaeab700e686d8a68ee
Mihajlo Mihajlovic <mihajlo.mihajlovic@fazi.rs>
2022-02-16 18:48:49 +0100

    New config.json file for baseAPI address.

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/_index.scss';
 import App from './App';
+import { setBaseApi } from './scripts/routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+fetch('./config.json').then(response => {
+    response.json().then(json => {
+        setBaseApi(json.baseAPI)
+        ReactDOM.render(<App />, document.getElementById('root'));
+    });
+});
+
--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -1,25 +1,22 @@
-export const API = {
-	signalRChannel: 'http://35.225.80.47:8080/strategyState',
-	arbitrageStrategies: 'http://35.225.80.47:8080/strategies',
-	arbitrageProduct: 'http://35.225.80.47:8080/products/',
-	startStopStrategy: 'http://35.225.80.47:8080/strategies/active/1/',
-	loadStrategy: 'http://35.225.80.47:8080/strategies/load/',
-	getProductAliasFunctionNames: 'http://35.225.80.47:8080/products/productAliasFunctionNames',
-	getIbMarketDataTypes: 'http://35.225.80.47:8080/products/ib/marketDataTypes',
-	getProductDetails: 'http://35.225.80.47:8080/products/details/',
-	startCycle: 'http://35.225.80.47:8080/strategies/place/',
-	differential: 'http://35.225.80.47:8080/strategies/differential/',
-	tenminutes: 'http://35.225.80.47:8080/strategies/tenminutes/3/',
+var baseAPI = '';
+export var API = {
+	signalRChannel: '/strategyState',
+	arbitrageStrategies: '/strategies',
+	arbitrageProduct: '/products/',
+	startStopStrategy: '/strategies/active/1/',
+	loadStrategy: '/strategies/load/',
+	getProductAliasFunctionNames: '/products/productAliasFunctionNames',
+	getIbMarketDataTypes: '/products/ib/marketDataTypes',
+	getProductDetails: '/products/details/',
+	startCycle: '/strategies/place/',
+	differential: '/strategies/differential/',
+	tenminutes: '/strategies/tenminutes/3/',
+};
 
-
-	//LOCAL ENVIRONMENT
-	// trades: 'http://localhost:5000/trades',
-	// getStrategiesProgress: 'http://localhost:5000/strategies/',
-	// strategiesLimits: 'http://localhost:5000/strategies/limits/',
-	// getStrategiesParameters: 'http://localhost:5000/strategies/parameters/',
-	// updateStrategiesParameters: 'http://localhost:5000/strategies/parameters/',
-	// strategyStart: 'http://localhost:5000/strategies/start/',
-	// strategyStop: 'http://localhost:5000/strategies/stop/',
-	// signalRChannel: 'http://localhost:5000/strategyState',
-	// getTrades: 'http://localhost:5000/strategyState',
+export const setBaseApi = data => {
+	baseAPI = data;
+	for (let route in API) {
+		API[route] =  data + API[route];
+		console.log(route);
+	}
 };
